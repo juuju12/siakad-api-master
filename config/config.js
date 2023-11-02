ca1c7e37cd62d3a9b7b9a92c717f7c83ce9cf99f
juuju12 <96474358+juuju12@users.noreply.github.com>
2023-11-02 20:20:16 +0700

    config.js

--- config/config.js
+++ config/config.js
@@ -4,12 +4,12 @@ dotenv.config()
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DRIVER
+    username: "root",
+    password: "",
+    database: "siakad_api",
+    host: "127.0.0.1",
+    port: 3306,
+    dialect: "mysql"
   },
   test: {
     username: process.env.CI_DB_USERNAME,
