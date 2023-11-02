0ecf5c3df7e718809aca501a55497775ea735881
juuju12 <96474358+juuju12@users.noreply.github.com>
2023-11-02 20:13:26 +0700

    index.js

--- router/index.js
+++ router/index.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const exampleController = require("../controller/exampleController");
+const routeMahasiswa = require("./mahasiswa");
+const routeDosen = require("./dosen");
+const routeMatkul = require("./matkul");
+const routeDosenMatkul = require("./dosenmatkul");
+const routemahasiswabimbingan = require("./mahasiswabimbingan");
+const routejadwalmatakuliah = require("./jadwalmatakuliah");
 const route = express.Router()
 
 route.get('/',exampleController.index)
-
+route.use('/mahasiswa',routeMahasiswa)
+route.use('/dosen',routeDosen)
+route.use('/matkul',routeMatkul)
+route.use('/dosen-matkul',routeDosenMatkul)
+route.use('/mahasiswa-bimbingan',routemahasiswabimbingan)
+route.use('/jadwal-matkul',routejadwalmatakuliah)
 module.exports = route
